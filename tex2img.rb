2371d4086f10e7cd077a69f1eaab58cb008eb07d
Watson <wtsnjp@gmail.com>
2017-09-30 18:34:35 +0900

    update for tex2img 2.1.9

--- tex2img.rb
+++ tex2img.rb
@@ -2,10 +2,10 @@ class Tex2img < Formula
   desc "Compile TeX file to various formats of image"
   homepage "http://island.geocities.jp/loveinequality/"
   if OS.mac?
-    url "http://island.geocities.jp/loveinequality/tex2imgcMac2.1.8.zip"
-    sha256 "398fe7ad5ddc429f2111b3538f854cace5acae38d6cdbb7751e2230948af3334"
+    url "http://island.geocities.jp/loveinequality/tex2imgcMac2.1.9.zip"
+    sha256 "d594d93be94391aeafd3e7ba3a10f74a70dcf7a743216f30efd9a5652d0c4542"
   end
-  version "2.1.8"
+  version "2.1.9"
 
   def install
     bin.install "tex2img"
